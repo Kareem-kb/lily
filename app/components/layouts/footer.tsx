e81559ec63221503a1371220a97ca31b327a2cc6
Kareem Bakarbashat <thought.kb@gmail.com>
2025-09-14 19:35:45 +0300

    css-fixing-3

--- app/components/layouts/footer.tsx
+++ app/components/layouts/footer.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
-import InstagramIcon from '../../../public/icons/instagram-icon.svg';
-import TiktokIcon from '../../../public/icons/tiktok-icon.svg';
-import WhatsappIcon from '../../../public/icons/whatsapp-icon.svg';
+import InstagramIcon from '../icons/instagram-icon.svg';
+import TiktokIcon from '../icons/tiktok-icon.svg';
+import WhatsappIcon from '../icons/whatsapp-icon.svg';
 
 export default function Footer() {
   return (
